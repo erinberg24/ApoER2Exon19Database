ef60bac0a7557c4b831cccff529accb0c330fecf
Dhanatt Horatanachai <dhanatts@bu.edu>
2020-04-23 21:22:11 -0400

    update login page

--- login-form.py
+++ login-form.py
@@ -1,18 +1,58 @@
 #!/usr/local/Python-3.7/bin/Python
+import pymysql
+import sys
 import cgi
 import cgitb
 cgitb.enable()
 
-print("Content-type:text/html\n")
+def header():
+    print("Content-type:text/html\n")
 
-print("""<html><title>LOGIN</title>
-<header>Please login</header>
-<body>""")
+    print("""<html><head>
+    <title>LOGIN</title>
+    <style>
+        body{font-family:"Times New Roman"; font-size:15px;}
+        #login{
+            border-style:ridge;
+            border-color:#fa8072;
+            padding: 15px;
+            text-align: center;
+            font-size: 20px;
+        }
+    </style></head>
+    """)
 
-print("""<form action = "" method="post">
-Username: <input type = "text" name="name"  required/><br />
-Password: <input type = "text" name="password" reqired/><br />
-<input type="submit" value="Submit" />
-</form>""")
+def log_in():
+    print("""<div id = login>
+    <h1>Please login</h1>
+    <form method="POST" action="" id=login_form>
+    <p>Username: <input type = "text" name="name"  required/></p>
+    <p>Password: <input type = "text" name="password" reqired/></p>
+    <p> <input type="submit" value="Submit"> </p>
+    </form></div>""")
 
+form = cgi.FieldStorage()
+if form:
+    header()
+    try:
+        name = str(form.getvalue("Username"))
+        pwd = str(form.getvalue("Password"))
 
+        pwdbin = password.encode()
+    header()
+    print("<meta http-equiv="refresh" content="0")
+    </head>
+    <body>")
+
+else:
+    print("</head><center><font color = "red"><h2>Please Try again</h2></font></center>")
+    login()
+
+except Exception as exc:
+    print("Error:", str(e))
+
+else:
+    header()
+    login()
+
+print("</body></html>")
