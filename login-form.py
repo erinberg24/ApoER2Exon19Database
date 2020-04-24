7075757ef098c14f4457bf0fb0e0b1b2b892c40d
Dhanatt Horatanachai <dhanatts@bu.edu>
2020-04-24 19:51:58 -0400

    update

--- login-form.py
+++ login-form.py
@@ -1,58 +1,52 @@
 #!/usr/local/Python-3.7/bin/Python
 import pymysql
 import sys
+import re
+import hashlib
 import cgi
 import cgitb
 cgitb.enable()
 
-def header():
-    print("Content-type:text/html\n")
+print("Content-type:text/html\n")
 
-    print("""<html><head>
-    <title>LOGIN</title>
-    <style>
-        body{font-family:"Times New Roman"; font-size:15px;}
-        #login{
-            border-style:ridge;
-            border-color:#fa8072;
-            padding: 15px;
-            text-align: center;
-            font-size: 20px;
+print("""<html><head>
+<title>LOGIN</title>
+<style>
+    body{font-family:"Times New Roman"; font-size:15px;}
+    #login{
+        border-style:ridge;
+        border-color:#fa8072;
+        padding: 15px;
+        text-align: center;
+        font-size: 20px;
         }
-    </style></head>
-    """)
+</style></head>
+""")
 
-def log_in():
-    print("""<div id = login>
-    <h1>Please login</h1>
-    <form method="POST" action="" id=login_form>
-    <p>Username: <input type = "text" name="name"  required/></p>
-    <p>Password: <input type = "text" name="password" reqired/></p>
-    <p> <input type="submit" value="Submit"> </p>
-    </form></div>""")
+print("""<div id = login>
+<h1>Please login</h1>
+<form method="POST" action="" id=login_form>
+<p>Username: <input type = "text" name="name"  required/></p>
+<p>Password: <input type = "text" name="password" reqired/></p>
+<p> <input type="submit" value="Submit"> </p>
+</form></div>""")
 
 form = cgi.FieldStorage()
-if form:
-    header()
-    try:
-        name = str(form.getvalue("Username"))
-        pwd = str(form.getvalue("Password"))
+name = str(form.getvalue("Username"))
+pwd = str(form.getvalue("Password"))
 
-        pwdbin = password.encode()
-    header()
-    print("<meta http-equiv="refresh" content="0")
-    </head>
-    <body>")
-
-else:
-    print("</head><center><font color = "red"><h2>Please Try again</h2></font></center>")
-    login()
-
-except Exception as exc:
-    print("Error:", str(e))
-
-else:
-    header()
-    login()
+if pwd:
+    pwd = pwd.encode()
+    pwd = hashlib.md5(pwd).hexdigest()
+    query1 = '''SELECT user, password FROM UP WHERE user = "%s" and password = "%s"'''.format(name,pwd)
+    log = execute_query(query1)
+    print(log)
+    if len(log) == 0:
+        print("Error page")
+    else:
+        if log[0][2]:
+            print("Welcome to ApoER2 Database webpage".format(name))
+        else:
+            print('Please try again')
 
 print("</body></html>")
