3b770953dd02bac08772bd442771d67c3307c17a
Dhanatt Horatanachai <dhanatts@bu.edu>
2020-04-26 15:16:57 -0400

    reference

---
+++ Reference.py
@@ -0,0 +1,17 @@
+#!/usr/local/Python-3.7/bin/Python
+import pymysql
+import sys
+import cgi
+import cgitb
+
+cgitb.enable()
+print("Content-type:text/html\n")
+
+print("<html><head>")
+print("<title>Reference</title>")
+
+print("<style>
+
+</style></head>")
+
+print("<h1>References</h1>)
